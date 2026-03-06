ea696d5c1cedb401bab17c1e9ec0d9843820abd1
CLAMALELI <155554344+CLAMALELI@users.noreply.github.com>
2026-03-06 14:51:28 +0100

    Update sonde_memory.sh

--- sonde_memory.sh
+++ sonde_memory.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-while true; do
-    date=$(date '+%Y-%m-%d %H:%M:%S')
-    total=$(df -h / | awk 'NR==2 {print $5}')
-    echo "[$date] [DISQUE] Utilisation Globale: $total"
-done
+date=$(date '+%Y-%m-%d %H:%M:%S')
+total=$(df -h / | awk 'NR==2 {print $5}')
+echo "[$date] [DISQUE] Utilisation Globale: $total"
