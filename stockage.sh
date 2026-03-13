f88355463f277c55c9516f4c8ff5f403d36ab3e2
CLAMALELI <155554344+CLAMALELI@users.noreply.github.com>
2026-03-13 14:27:18 +0100

    Update stockage.sh

--- stockage.sh
+++ stockage.sh
@@ -8,6 +8,4 @@ CPU=$(bash $PROJET/sonde_cpu.sh)
 RAM=$(python3 $PROJET/sonde_RAM.py)
 MEMORY=$(bash $PROJET/sonde_memory.sh)
 
-echo "DEBUG - CPU: '$CPU', RAM: '$RAM', MEMORY: '$MEMORY'"
-
 rrdtool update $RRD N:$CPU:$RAM:$MEMORY
