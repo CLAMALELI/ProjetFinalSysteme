b8d3a69aa061f537153305b006237a8d6a34b436
CLAMALELI <155554344+CLAMALELI@users.noreply.github.com>
2026-03-13 13:55:41 +0100

    Update stockage.sh

--- stockage.sh
+++ stockage.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+# chemins
+PROJET="/home/matteojaubert/ProjetFinalSysteme"
+RRD="$PROJET/system.rrd"
+
+CPU=$(bash $PROJET/sonde_CPU.sh)
+RAM=$(python3 $PROJET/sonde_RAM.py)
+MEMORY=$(bash $PROJET/sonde_memory.sh)
+
+rrdtool update $RRD N:$CPU:$RAM:$MEMORY
