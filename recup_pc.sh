6f33e0ae79df5f9722f604cb7cc7bcc5d40e380e
CLAMALELI <155554344+CLAMALELI@users.noreply.github.com>
2026-03-19 17:27:28 +0100

    Update recup_pc.sh

--- recup_pc.sh
+++ recup_pc.sh
@@ -4,7 +4,7 @@
 PC="uapv2401103@pedago.univ-avignon.fr"
 RRD="/home/matteojaubert/ProjetFinalSysteme/system.rrd"
 
-CPU=$(ssh -i ~/.ssh/id_rsa $PC "bash ~/ProjetFinalSysteme/sonde_CPU.sh")
+CPU=$(ssh -i ~/.ssh/id_rsa $PC "bash ~/ProjetFinalSysteme/sonde_cpu.sh")
 RAM=$(ssh -i ~/.ssh/id_rsa $PC "python3 ~/ProjetFinalSysteme/sonde_RAM.py")
 MEMORY=$(ssh -i ~/.ssh/id_rsa $PC "bash ~/ProjetFinalSysteme/sonde_memory.sh")
 
