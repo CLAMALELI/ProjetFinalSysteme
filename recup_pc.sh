d11b8c8e143c4a07dd78f8eb9083a25c8786044b
CLAMALELI <155554344+CLAMALELI@users.noreply.github.com>
2026-03-19 17:19:26 +0100

    Update recup_pc.sh

--- recup_pc.sh
+++ recup_pc.sh
@@ -2,9 +2,10 @@
 
 # Adresse de ton PC
 PC="uapv2401103@pedago.univ-avignon.fr"
+RRD="/home/matteojaubert/ProjetFinalSysteme/system.rrd"
 
 CPU=$(ssh -i ~/.ssh/id_rsa $PC "bash ~/ProjetFinalSysteme/sonde_CPU.sh")
 RAM=$(ssh -i ~/.ssh/id_rsa $PC "python3 ~/ProjetFinalSysteme/sonde_RAM.py")
-DISK=$(ssh -i ~/.ssh/id_rsa $PC "bash ~/ProjetFinalSysteme/sonde_memory.sh")
+MEMORY=$(ssh -i ~/.ssh/id_rsa $PC "bash ~/ProjetFinalSysteme/sonde_memory.sh")
 
 rrdtool update $RRD N:$CPU:$RAM:$MEMORY
