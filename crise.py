f5aab421409c0098dbccda960f0569de63769ff6
CLAMALELI <155554344+CLAMALELI@users.noreply.github.com>
2026-03-19 18:57:08 +0100

    Update crise.py

--- crise.py
+++ crise.py
@@ -68,7 +68,7 @@ def get_data_from_json():
         "cpu": round(last_entry[0], 2),
         "ram": round(last_entry[1], 2),
         "disk": round(last_entry[2], 2),
-        "origine": round(last_entry[3], 3),
+        "origine": "PC" if round(last_entry[3], 0) == 1 else "VM",
     }
 
 def generate_graph():
