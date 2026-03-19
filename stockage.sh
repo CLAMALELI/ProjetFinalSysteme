7b1a68b0e3d697741f584e8046dbf3f869bbaaf9
CLAMALELI <155554344+CLAMALELI@users.noreply.github.com>
2026-03-19 17:59:36 +0100

    Update stockage.sh

--- stockage.sh
+++ stockage.sh
@@ -8,5 +8,6 @@ RRD="/home/matteojaubert/ProjetFinalSysteme/system.rrd"
 CPU=$(bash $PROJET/sonde_cpu.sh)
 RAM=$(python3 $PROJET/sonde_RAM.py)
 MEMORY=$(bash $PROJET/sonde_memory.sh)
+ORIGINE=0  # 0 pour VM
 
-rrdtool update $RRD N:$CPU:$RAM:$MEMORY
+rrdtool update $RRD N:$CPU:$RAM:$MEMORY:$ORIGINE
