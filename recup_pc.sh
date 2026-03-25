957798b71a19a81ccf47d1409caf40f914c0261b
CLAMALELI <155554344+CLAMALELI@users.noreply.github.com>
2026-03-25 12:38:13 +0100

    Update recup_pc.sh

--- recup_pc.sh
+++ recup_pc.sh
@@ -7,6 +7,6 @@ RRD="/home/matteojaubert/ProjetFinalSysteme/system.rrd"
 CPU=$(ssh -i ~/.ssh/id_rsa $PC "bash ~/ProjetFinalSysteme/sonde_cpu.sh")
 RAM=$(ssh -i ~/.ssh/id_rsa $PC "bash ~/ProjetFinalSysteme/sonde_RAM.sh")
 MEMORY=$(ssh -i ~/.ssh/id_rsa $PC "bash ~/ProjetFinalSysteme/sonde_memory.sh")
-ORIGINE=1  # 1 pour VM
+ORIGINE=1  # 1 pour PC
 
 rrdtool update $RRD N:$CPU:$RAM:$MEMORY:$ORIGINE
