295d290807a5a27d5857a24a6adfd59ac681b31e
CLAMALELI <155554344+CLAMALELI@users.noreply.github.com>
2026-03-25 12:50:18 +0100

    Update stockage.sh

--- stockage.sh
+++ stockage.sh
@@ -3,6 +3,7 @@
 # chemins
 PROJET="/home/matteojaubert/ProjetFinalSysteme"
 RRD="/home/matteojaubert/ProjetFinalSysteme/system.rrd"
+GRAPH_PATH="$PROJET/graph0.png"
 
 # --- MACHINE 1 (VM)
 CPU=$(bash $PROJET/sonde_cpu.sh)
@@ -11,3 +12,18 @@ MEMORY=$(bash $PROJET/sonde_memory.sh)
 ORIGINE=0  # 0 pour VM
 
 rrdtool update $RRD N:$CPU:$RAM:$MEMORY:$ORIGINE
+
+rrdtool graph $GRAPH_PATH \
+  --start -1h \
+  --title "Utilisation des ressources (1h)" \
+  --vertical-label "%" \
+  --lower-limit 0 \
+  --upper-limit 100 \
+  DEF:cpu=$RRD:cpu:AVERAGE \
+  DEF:ram=$RRD:ram:AVERAGE \
+  DEF:disk=$RRD:memory:AVERAGE \
+  DEF:origine=$RRD:origine:AVERAGE \
+  LINE1:cpu#FF0000:"CPU" \
+  LINE1:ram#00FF00:"RAM" \
+  LINE1:disk#0000FF:"DISK" \
+  LINE1:origine#FFFF00:"ORIGINE"
